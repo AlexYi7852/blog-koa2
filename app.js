ee09c90304267e9fba0dacfac41abdf17daeef1d
易烨 <yiye@yiyedeMacBook-Pro.local>
2020-08-16 00:55:15 +0800

    处理cookie、session连接redis

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
 
+const blog = require('./routes/blog')
+const user = require('./routes/user')
 const index = require('./routes/index')
 const users = require('./routes/users')
+
+const { REDIS_CONFIG } = require('./config/db')
 
 // error handler
 onerror(app)
@@ -31,8 +37,24 @@ app.use(async (ctx, next) => {
   const ms = new Date() - start
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
+// session 密匙
+app.keys = ['WJios___--7852#']
+app.use(session({
+  // 配置cookie
+  cookie: {
+    path: '/', // 默认配置
+    httpOnly: true, // 默认配置、限制前端修改 cookie
+    maxAge: 24 * 60 * 60 * 1000 // 过期时间
+  },
+  // 配置 redis
+  store: redisStore({
+    all: `${ REDIS_CONFIG.host }:${ REDIS_CONFIG.port }`
+  })
+}))
 
 // routes
+app.use(blog.routes(), blog.allowedMethods())
+app.use(user.routes(), user.allowedMethods())
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
 
